4d23dea464da494cd7247bb3204b53eeb1e9e596
David <davidobob@laposte.net>
2020-12-06 09:50:25 +0100

    all basic infos displayed and link added'

--- app/javascript/components/Card.jsx
+++ app/javascript/components/Card.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../style/card.css";
+import { Link } from "react-router-dom";
 
 export default function Card(props) {
   const { name, url } = props;
   const [pokeTypes, setPokeTypes] = useState([]);
   const [image, setImage] = useState("");
+  const [pokeId, setPokeId] = useState("");
 
   useEffect(() => {
     let value = [];
     axios.get(url).then((r) => {
+      //   console.log(r.data);
       setImage(r.data.sprites.front_default);
+      setPokeId(r.data.id);
       for (const key in r.data.types) {
         value.push(r.data.types[key].type.name);
       }
@@ -18,40 +22,26 @@ export default function Card(props) {
     });
   }, []);
 
-  //   const fetchImage = () => {
-  //     console.log("je suis dans fetch image");
-  //     axios.get(url).then((r) => {
-  //       //   console.log(r.data.types);
-  //       setImage(r.data.sprites.front_default);
-  //       setTypes(r.data.types);
-  //       //   for (const key in r.data.types) {
-  //       //     console.log("OUI");
-  //       //     // console.log(r.data.types[key].type.name);
-  //       //     // setTypes([r.data.sprites.front_default]);
-  //       //   }
-  //     });
-  //   };
-
-  //   const pokemonCard = () => {
-  //     fetchImage(url);
-  //     console.log(types);
-  //     return (
-  //       <Fragment>
-  //         <p>{name}</p>
-  //         <p>{types}</p>
-  //         <img src={image} alt={name} />
-  //       </Fragment>
-  //     );
-  //   };
+  const stringifyPokeId = () => {
+    const number = pokeId.toString().padStart(4, "0");
+    return number;
+  };
 
   return (
-    <div className={`pokemon-card ${pokeTypes[0]}`} key={name}>
-      {console.log(name, pokeTypes)}
-      <p className="name">{name}</p>
-      {pokeTypes.map((type) => {
-        return <p className={`type ${pokeTypes[0]}`}>{type}</p>;
-      })}
-      <img className="image" src={image} alt={name} />
-    </div>
+    <Link to={`/pokemons/${pokeId}`} className="link">
+      <div className={`pokemon-card ${pokeTypes[0]}`} key={name}>
+        {/* {console.log(name, pokeTypes)} */}
+        <p className="name">{name}</p>
+        {pokeTypes.map((type) => {
+          return (
+            <p key={type} className={`type ${pokeTypes[0]}`}>
+              {type}
+            </p>
+          );
+        })}
+        <p className="id">{`#${stringifyPokeId()}`}</p>
+        <img className="image" src={image} alt={name} />
+      </div>
+    </Link>
   );
 }
